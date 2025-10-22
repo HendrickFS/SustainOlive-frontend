4d07c2e2aa865d4cf46f69ffe3c83d1f15328521
HendrickFS <hendrickfel@gmail.com>
2025-10-22 22:03:22 +0100

    feat: env file update

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -8,13 +8,13 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAN9_UF8KXD8-vBYCo6H4L0kE6w4UZisho",
-  authDomain: "sustainolive-auth.firebaseapp.com",
-  projectId: "sustainolive-auth",
-  storageBucket: "sustainolive-auth.firebasestorage.app",
-  messagingSenderId: "311485292725",
-  appId: "1:311485292725:web:50fa3bb9f6210d62779efc",
-  measurementId: "G-CKT2LNQCLD"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
